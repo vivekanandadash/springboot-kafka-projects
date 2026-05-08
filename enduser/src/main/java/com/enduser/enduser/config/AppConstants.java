2c0e196319f444be70e7c6ad3be551ae890242ac
Vivekananda <vivekanandadash245@gmail.com>
2026-05-08 17:49:45 +0530

    config the application properties file for consuming the messages  from producer with adding the groupid and topic for getting the the messgaes

---
+++ enduser/src/main/java/com/enduser/enduser/config/AppConstants.java
@@ -0,0 +1,7 @@
+package com.enduser.enduser.config;
+
+public class AppConstants {
+    public static final String LOCATION_UPDATE_TOPIC = "location-topic-update";
+    public static final String GROUP_ID = "group-1";
+
+}
