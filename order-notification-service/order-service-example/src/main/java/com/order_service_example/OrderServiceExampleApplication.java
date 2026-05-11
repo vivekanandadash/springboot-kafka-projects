8d51fbd38c6514edab76be146361151163659715
Vivekananda <vivekanandadash245@gmail.com>
2026-05-11 12:44:01 +0530

    creating order-notification service structure using kafka

---
+++ order-notification-service/order-service-example/src/main/java/com/order_service_example/OrderServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.order_service_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceExampleApplication.class, args);
+	}
+
+}
---
+++ order-notification-service/order-service-example/src/test/java/com/order_service_example/OrderServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_service_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
