56cf0e66d5b3df477f1b56775d443cc8b5ee3fb1
Vivekananda <vivekanandadash245@gmail.com>
2026-05-12 12:57:27 +0530

    creating OrderEvent Dto so that we can get the data from json and convert to object'

--- order-notification-service/order-service-example/src/main/java/com/order_service_example/dto/OrderEvent.java
+++ order-notification-service/order-service-example/src/main/java/com/order_service_example/dto/OrderEvent.java
@@ -1,4 +1,31 @@
 package com.order_service_example.dto;
 
 public class OrderEvent {
+    private Long orderId;
+    private String  email;
+    private String status;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
