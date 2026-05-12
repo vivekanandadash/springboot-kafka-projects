b6df49c8a0a16f207353638fe311d0823b06ca86
Vivekananda <vivekanandadash245@gmail.com>
2026-05-12 13:10:19 +0530

    add NotificationConsumer to get the data

--- order-notification-service/notification-service/src/main/java/com/notification_service/kafka/NotificationConsumer.java
+++ order-notification-service/notification-service/src/main/java/com/notification_service/kafka/NotificationConsumer.java
@@ -1,4 +1,21 @@
 package com.notification_service.kafka;
 
+import com.notification_service.dto.OrderEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationConsumer {
+
+    @KafkaListener(
+            topics = "order-events",
+            groupId = "notification-group"
+    )
+    public void consume(OrderEvent event) {
+
+        System.out.println("Event Received");
+        System.out.println("Email: " + event.getEmail());
+        System.out.println("Order ID: " + event.getOrderId());
+        System.out.println("Status: " + event.getStatus());
+    }
 }
