8e03af86998ac2a10ed2a94ba968d91f2faa691f
Vivekananda <vivekanandadash245@gmail.com>
2026-05-12 12:56:29 +0530

    creating producer config file

--- order-notification-service/order-service-example/src/main/java/com/order_service_example/config/KafkaProducerConfig.java
+++ order-notification-service/order-service-example/src/main/java/com/order_service_example/config/KafkaProducerConfig.java
@@ -1,4 +1,52 @@
 package com.order_service_example.config;
 
+import com.order_service_example.dto.OrderEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, OrderEvent> producerFactory() {
+
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092"
+        );
+
+        config.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
+
+        config.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JacksonJsonSerializer.class
+        );
+
+        // IMPORTANT FIX
+        config.put(
+                JacksonJsonSerializer.ADD_TYPE_INFO_HEADERS,
+                false
+        );
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, OrderEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
