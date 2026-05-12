1c5535cf349631890b60ad0fdb930e50b538b7eb
Vivekananda <vivekanandadash245@gmail.com>
2026-05-12 13:07:01 +0530

    adding OrderEvent dto class

--- order-notification-service/notification-service/src/main/java/com/notification_service/dto/OrderEvent.java
+++ order-notification-service/notification-service/src/main/java/com/notification_service/dto/OrderEvent.java
@@ -1,4 +1,32 @@
 package com.notification_service.dto;
 
 public class OrderEvent {
+
+    private Long orderId;
+    private String email;
+    private String status;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
