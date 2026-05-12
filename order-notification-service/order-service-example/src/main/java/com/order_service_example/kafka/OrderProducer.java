49bb50615555cc6161e5eaee5d8b7e20f0215ceb
Vivekananda <vivekanandadash245@gmail.com>
2026-05-12 12:59:27 +0530

    creating Producer layer so that we can creating a kafka topic and send it to kafka server

--- order-notification-service/order-service-example/src/main/java/com/order_service_example/kafka/OrderProducer.java
+++ order-notification-service/order-service-example/src/main/java/com/order_service_example/kafka/OrderProducer.java
@@ -1,4 +1,22 @@
 package com.order_service_example.kafka;
 
+import com.order_service_example.dto.OrderEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderProducer {
+
+    private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    private static final String TOPIC = "order-events";
+
+    public OrderProducer(KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendOrderEvent(OrderEvent event) {
+        kafkaTemplate.send(TOPIC, event);
+        System.out.println("Event Sent : " + event);
+    }
 }
