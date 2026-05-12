959938ed5ccedd97e3a7043c55d888346dde0566
Vivekananda <vivekanandadash245@gmail.com>
2026-05-12 13:00:36 +0530

    Creating controller-layer so that we can return the response

--- order-notification-service/order-service-example/src/main/java/com/order_service_example/controller/OrderController.java
+++ order-notification-service/order-service-example/src/main/java/com/order_service_example/controller/OrderController.java
@@ -1,4 +1,25 @@
 package com.order_service_example.controller;
 
+import com.order_service_example.dto.OrderEvent;
+import com.order_service_example.kafka.OrderProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderProducer producer;
+
+    public OrderController(OrderProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping
+    public String placeOrder(@RequestBody OrderEvent event) {
+        producer.sendOrderEvent(event);
+        return "Order Event Published Successfully";
+    }
 }
