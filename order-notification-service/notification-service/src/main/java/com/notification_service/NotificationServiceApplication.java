433c620478ccf4ce0e8d066851aaef7ba34f5d1e
Vivekananda <vivekanandadash245@gmail.com>
2026-05-13 16:21:07 +0530

    adding main class

--- order-notification-service/notification-service/src/main/java/com/notification_service/NotificationServiceApplication.java
+++ order-notification-service/notification-service/src/main/java/com/notification_service/NotificationServiceApplication.java
@@ -2,6 +2,7 @@ package com.notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
