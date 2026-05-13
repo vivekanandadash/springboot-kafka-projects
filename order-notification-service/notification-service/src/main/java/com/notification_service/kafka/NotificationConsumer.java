abce55ad7702a6b2e22470cc063432aa20e7169c
Vivekananda <vivekanandadash245@gmail.com>
2026-05-13 16:24:26 +0530

    Creating Notification Consumer class for consuming the kafka topic

--- order-notification-service/notification-service/src/main/java/com/notification_service/kafka/NotificationConsumer.java
+++ order-notification-service/notification-service/src/main/java/com/notification_service/kafka/NotificationConsumer.java
@@ -1,11 +1,18 @@
 package com.notification_service.kafka;
 
 import com.notification_service.dto.OrderEvent;
+import com.notification_service.service.EmailService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationConsumer {
+
+    private final EmailService emailService;
+
+    public NotificationConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
 
     @KafkaListener(
             topics = "order-events",
@@ -13,9 +20,14 @@ public class NotificationConsumer {
     )
     public void consume(OrderEvent event) {
 
-        System.out.println("Event Received");
-        System.out.println("Email: " + event.getEmail());
-        System.out.println("Order ID: " + event.getOrderId());
-        System.out.println("Status: " + event.getStatus());
+        System.out.println("Order Event Received");
+
+        emailService.sendOrderEmail(
+                event.getEmail(),
+                event.getCustomerName(),
+                event.getOrderId(),
+                event.getStatus(),
+                event.getTotalAmount().toString()
+        );
     }
 }
