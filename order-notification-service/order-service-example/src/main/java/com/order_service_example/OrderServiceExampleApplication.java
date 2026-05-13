66cb2c618c8d4342320631505efc5a3e57c5ad46
Vivekananda <vivekanandadash245@gmail.com>
2026-05-13 16:32:58 +0530

    adding @EnableKafka Annotation in main method for enabling kafka for puslishing messages

--- order-notification-service/order-service-example/src/main/java/com/order_service_example/OrderServiceExampleApplication.java
+++ order-notification-service/order-service-example/src/main/java/com/order_service_example/OrderServiceExampleApplication.java
@@ -2,8 +2,10 @@ package com.order_service_example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class OrderServiceExampleApplication {
 
 	public static void main(String[] args) {
