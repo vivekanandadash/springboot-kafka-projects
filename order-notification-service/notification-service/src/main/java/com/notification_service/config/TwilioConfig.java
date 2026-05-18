2bd1cd03f24730986eb6cab885f2a33b0090f114
Vivekananda <vivekanandadash245@gmail.com>
2026-05-18 17:03:27 +0530

    adding twillio config file

--- order-notification-service/notification-service/src/main/java/com/notification_service/config/TwilioConfig.java
+++ order-notification-service/notification-service/src/main/java/com/notification_service/config/TwilioConfig.java
@@ -1,4 +1,24 @@
 package com.notification_service.config;
 
+import com.twilio.Twilio;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class TwilioConfig {
+
+    @Value("${twilio.account-sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth-token}")
+    private String authToken;
+
+    @PostConstruct
+    public void initTwilio() {
+
+        Twilio.init(accountSid, authToken);
+
+        System.out.println("Twilio Initialized Successfully");
+    }
 }
