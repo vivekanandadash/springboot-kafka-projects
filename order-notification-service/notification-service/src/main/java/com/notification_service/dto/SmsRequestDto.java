ecc8da75cf61a5a44397e95d69042eac431e2ef0
Vivekananda <vivekanandadash245@gmail.com>
2026-05-18 17:16:40 +0530

    adding dto class for using later for scalabilty design

--- order-notification-service/notification-service/src/main/java/com/notification_service/dto/SmsRequestDto.java
+++ order-notification-service/notification-service/src/main/java/com/notification_service/dto/SmsRequestDto.java
@@ -1,4 +1,32 @@
 package com.notification_service.dto;
 
 public class SmsRequestDto {
+
+    private String to;
+
+    private String message;
+
+    public SmsRequestDto() {
+    }
+
+    public SmsRequestDto(String to, String message) {
+        this.to = to;
+        this.message = message;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- order-notification-service/notification-service/src/main/java/com/notification_service/dto/WhatsappRequestDto.java
+++ order-notification-service/notification-service/src/main/java/com/notification_service/dto/WhatsappRequestDto.java
@@ -1,4 +1,32 @@
 package com.notification_service.dto;
 
 public class WhatsappRequestDto {
+
+    private String to;
+
+    private String message;
+
+    public WhatsappRequestDto() {
+    }
+
+    public WhatsappRequestDto(String to, String message) {
+        this.to = to;
+        this.message = message;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
